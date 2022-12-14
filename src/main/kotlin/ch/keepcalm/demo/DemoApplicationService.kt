fb5f3d7c21ee2b3d9504774a3d012899272d59de
Marcel Widmer <marzelwidmer@gmail.com>
2022-12-14 15:00:56 +0100

    WIP

--- src/main/kotlin/ch/keepcalm/demo/DemoApplicationService.kt
+++ src/main/kotlin/ch/keepcalm/demo/DemoApplicationService.kt
@@ -1,12 +1,20 @@
 package ch.keepcalm.demo
 
+import org.camunda.bpm.engine.RuntimeService
+import org.camunda.bpm.engine.delegate.DelegateExecution
+import org.camunda.bpm.engine.delegate.JavaDelegate
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.hateoas.config.EnableHypermediaSupport
 import org.springframework.hateoas.support.WebStack
+import org.springframework.stereotype.Component
 import org.springframework.web.server.adapter.ForwardedHeaderTransformer
+
 
 @SpringBootApplication
 @EnableHypermediaSupport(stacks = [WebStack.WEBFLUX], type = [EnableHypermediaSupport.HypermediaType.HAL])
@@ -19,6 +27,35 @@ class DemoApplicationService {
     }
 }
 
+
+
+
 fun main(args: Array<String>) {
     runApplication<DemoApplicationService>(*args)
+}
+
+
+
+@Component
+class MyFooBean() : JavaDelegate {
+    override fun execute(p0: DelegateExecution?) {
+        println("-------------> Hello from MyFooBean")
+    }
+}
+
+@Component
+class MyBarBean() : JavaDelegate {
+    override fun execute(p0: DelegateExecution?) {
+        println("-------------> Hello from MyBarBean")
+    }
+}
+
+@Component
+class Starter (private var runtimeService: RuntimeService) : InitializingBean {
+
+    @Throws(Exception::class)
+    override fun afterPropertiesSet() {
+        println("----------------------------------///////////////>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> START ")
+        runtimeService.startProcessInstanceByKey("MyFooBarProcess")
+    }
 }
