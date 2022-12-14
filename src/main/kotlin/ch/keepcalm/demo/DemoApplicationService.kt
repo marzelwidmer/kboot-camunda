447784d86ec2586daa70fe1d218430fe5c77e3c4
Marcel Widmer <marzelwidmer@gmail.com>
2022-12-14 16:10:52 +0100

    save

--- src/main/kotlin/ch/keepcalm/demo/DemoApplicationService.kt
+++ src/main/kotlin/ch/keepcalm/demo/DemoApplicationService.kt
@@ -1,16 +1,15 @@
 package ch.keepcalm.demo
 
-import org.camunda.bpm.engine.RuntimeService
-import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.RepositoryService
+import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.delegate.JavaDelegate
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication
-import org.springframework.beans.factory.InitializingBean
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.beans
 import org.springframework.hateoas.config.EnableHypermediaSupport
 import org.springframework.hateoas.support.WebStack
 import org.springframework.stereotype.Component
@@ -31,47 +30,43 @@ class DemoApplicationService {
 
 
 fun main(args: Array<String>) {
-    runApplication<DemoApplicationService>(*args).addApplicationListener {
-//        addInitializers(
-//            beans {
-//                bean {
-//                    ApplicationRunner { println("ApplicationRunner ----------------->") }
-//                }
-//            }
-//        )
+    runApplication<DemoApplicationService>(*args) {
+        addInitializers(
+            beans {
+                bean {
+                    ApplicationRunner {
+                        println("ApplicationRunner ----------------->")
+                        val repositoryService = ref<RepositoryService>()
+                        repositoryService.createDeployment()
+                            .addClasspathResource("franchise_change.bpmn")
+                            .deploy()
+
+
+                    }
+                }
+            }
+        )
     }
 }
 
-
-
 @Component
 class MyFooBean() : JavaDelegate {
-    override fun execute(p0: DelegateExecution?) {
+    override fun execute(delegate: DelegateExecution?) {
         println("-------------> Hello from MyFooBean")
     }
 }
 
 @Component
 class MyBarBean() : JavaDelegate {
-    override fun execute(p0: DelegateExecution?) {
+    override fun execute(delegate: DelegateExecution?) {
         println("-------------> Hello from MyBarBean")
     }
 }
 
-@Component
-class Starter (private var runtimeService: RuntimeService, private val repositoryService: RepositoryService) : InitializingBean {
 
-
-    @Throws(Exception::class)
-    override fun afterPropertiesSet() {
-        repositoryService.createDeployment()
-            .addClasspathResource("franchise_change.bpmn")
-            .deploy();
-
-
-
-        println("----------------------------------///////////////>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> START ")
-        runtimeService.startProcessInstanceById("MyFooBarProcess")
-//        startProcessInstanceByKey("MyFooBarProcess")
+class CalculateInterestService : JavaDelegate {
+    override fun execute(delegate: DelegateExecution) {
+        println("Spring Bean invoked.")
     }
 }
+
