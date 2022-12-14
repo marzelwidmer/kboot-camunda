c287c677ee1445c41b83c8d289bcd37efd793029
Marcel Widmer <marzelwidmer@gmail.com>
2022-12-14 16:54:40 +0100

    save

--- src/main/kotlin/ch/keepcalm/demo/DemoApplicationService.kt
+++ src/main/kotlin/ch/keepcalm/demo/DemoApplicationService.kt
@@ -1,32 +1,17 @@
 package ch.keepcalm.demo
 
-import org.camunda.bpm.engine.RepositoryService
 import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.delegate.JavaDelegate
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.beans
-import org.springframework.hateoas.config.EnableHypermediaSupport
-import org.springframework.hateoas.support.WebStack
 import org.springframework.stereotype.Component
-import org.springframework.web.server.adapter.ForwardedHeaderTransformer
 
 @SpringBootApplication
-@EnableHypermediaSupport(stacks = [WebStack.WEBFLUX], type = [EnableHypermediaSupport.HypermediaType.HAL])
 @EnableProcessApplication
-class DemoApplicationService {
-
-    @Bean
-    fun forwardedHeaderTransformer(): ForwardedHeaderTransformer? {
-        return ForwardedHeaderTransformer()
-    }
-}
-
-
+class DemoApplicationService
 
 
 fun main(args: Array<String>) {
@@ -36,10 +21,10 @@ fun main(args: Array<String>) {
                 bean {
                     ApplicationRunner {
                         println("ApplicationRunner ----------------->")
-                        val repositoryService = ref<RepositoryService>()
-                        repositoryService.createDeployment()
-                            .addClasspathResource("franchise_change.bpmn")
-                            .deploy()
+//                        val repositoryService = ref<RepositoryService>()
+//                        repositoryService.createDeployment()
+//                            .addClasspathResource("franchise_change.bpmn")
+//                            .deploy()
 
 
                     }
@@ -60,13 +45,6 @@ class MyFooBean() : JavaDelegate {
 class MyBarBean() : JavaDelegate {
     override fun execute(delegate: DelegateExecution?) {
         println("-------------> Hello from MyBarBean")
-    }
-}
-
-
-class CalculateInterestService : JavaDelegate {
-    override fun execute(delegate: DelegateExecution) {
-        println("Spring Bean invoked.")
     }
 }
 
